21549aa79dc72d9b9ea764ad252dfabe826d1ac0
THOMAJ32 <jeremy.thomas@partners.basf.com>
2025-03-06 13:48:08 +0100

    fix villes-voisines

--- app/api/neighbors/[postal_code]/route.ts
+++ app/api/neighbors/[postal_code]/route.ts
@@ -5,28 +5,29 @@ export async function GET(
     _: NextRequest,
     props: { params: Promise<{ postal_code: string }> }
 ) {
-    return wrapApiCall(
-        async () =>
-            await fetch(
-                `https://www.villes-voisines.fr/getcp.php?cp=${
-                    (
-                        await props.params
-                    ).postal_code
-                }&rayon=30`
-            )
-                .then((res) =>
-                    res
-                        .json()
-                        .then((neighbors: { code_postal: string }[]) =>
-                            Array.from(
-                                new Set(
-                                    Object.values(neighbors).map((neighbor) =>
-                                        neighbor.code_postal.slice(0, 2)
-                                    )
+    return wrapApiCall(async () =>
+        fetch(
+            `https://www.villes-voisines.fr/getcp.php?cp=${
+                (await props.params).postal_code
+            }&rayon=30`
+        )
+            .then((res) => {
+                console.log("res", res);
+                return res
+                    .json()
+                    .then((neighbors: { code_postal: string }[]) => {
+                        console.log("neighbors", neighbors);
+                        Array.from(
+                            new Set(
+                                Object.values(neighbors).map((neighbor) =>
+                                    neighbor.code_postal.slice(0, 2)
                                 )
                             )
-                        )
-                )
-                .catch((error: unknown) => console.log(error))
+                        );
+                    });
+            })
+            .catch((error: unknown) =>
+                console.log("error-ville-voisines", error)
+            )
     );
 }
